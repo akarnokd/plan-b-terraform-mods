95e0d10a2991c2aa30f9eb81fa6bc64dfefbfeea
akarnokd <akarnokd@gmail.com>
2023-01-26 10:37:27 +0100

    Ensure city names are distinct

--- FeatMoreCities/Plugin.cs
+++ FeatMoreCities/Plugin.cs
@@ -30,5 +30,37 @@ namespace FeatMoreCities
         {
             nb += cityCount.Value;
         }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SWorld_GenerationLua), nameof(SWorld_GenerationLua.GenerateCities))]
+        static void SWorld_GenerationLua_GenerateCities_Post()
+        {
+            // initial city names
+            List<string> cityNames = new(GGame.cityNames);
+            cityNames.Shuffle();
+
+            // if there are more cities to generate then names, cycle through the names again
+            // but append a counter
+            int counter = 2;
+            while (cityNames.Count < GGame.cities.Count)
+            {
+                var set = new List<string>(GGame.cityNames);
+                set.Shuffle();
+                for (int i = 0; i < set.Count; i++)
+                {
+                    set[i] = set[i] + " " + counter;
+                }
+
+                cityNames.AddRange(set);
+                counter++;
+            }
+
+            int j = 0;
+            foreach (var city in GGame.cities)
+            {
+                city.name = "New " + cityNames[j];
+                j++;
+            }
+        }
     }
 }
