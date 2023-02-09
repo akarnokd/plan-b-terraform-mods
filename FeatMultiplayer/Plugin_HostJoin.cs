4f60d6019e314e176a8996daf3144e7abfc0bcb2
akarnokd <akarnokd@gmail.com>
2023-02-09 14:14:22 +0100

    Run the server on the host

---
+++ FeatMultiplayer/Plugin_HostJoin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LibCommon;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+using static LibCommon.GUITools;
+
+namespace FeatMultiplayer
+{
+    public partial class Plugin : BaseUnityPlugin
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SSceneHud), "OnActivate")]
+        static void SSceneHud_OnActivate()
+        {
+            if (multiplayerMode == MultiplayerMode.MainMenu && hostMode.Value)
+            {
+                multiplayerMode = MultiplayerMode.Host;
+
+                StartServer();
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            stopNetwork.Cancel();
+
+            Thread.Sleep(2000); // FIXME find a better way
+        }
+
+    }
+}
