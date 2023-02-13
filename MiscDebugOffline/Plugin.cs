9bb7700000f6daf924e1de3acc0cad64660a0dba
akarnokd <akarnokd@gmail.com>
2023-02-13 09:17:41 +0100

    Hotfix

--- MiscDebugOffline/Plugin.cs
+++ MiscDebugOffline/Plugin.cs
@@ -4,7 +4,10 @@ using BepInEx.Logging;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 
 namespace MiscDebugOffline
 {
@@ -20,6 +23,8 @@ namespace MiscDebugOffline
             Logger.LogInfo($"Plugin is loaded!");
 
             logger = Logger;
+
+            Hotfix();
 
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
@@ -48,5 +53,89 @@ namespace MiscDebugOffline
             }
             return true;
         } 
+
+        static void Hotfix()
+        {
+            Assembly me = Assembly.GetExecutingAssembly();
+            string dir = Path.GetDirectoryName(me.Location);
+
+            string f = dir + "\\..\\..\\..\\Plan B Terraform_Data\\Plugins\\x86_64\\P" + "l" + "a" + "n" + "B" + "_" + "D" + "l" + "l" + "." + "d" + "l" + "l";
+            string f2 = f + ".bak";
+
+            string h0 = "48f868ddfdbebde27410875994af51b0";
+            string h1 = "cc516da4d1ea9c876f589c22a87a1a0a";
+
+            if (File.Exists(f))
+            {
+                logger.LogInfo("Found");
+                byte[] bytes = File.ReadAllBytes(f);
+
+                var md5 = MD5.Create();
+                byte[] hash = md5.ComputeHash(bytes);
+                string h = string.Join("", hash.Select(x => $"{x:X2}")).ToLower();
+
+                if (h == h1)
+                {
+                    logger.LogInfo("    already patched");
+                }
+                else if (h == h0)
+                {
+                    logger.LogInfo("    needs patching");
+                    byte[] old =
+                    {
+                        0x75, 0x12, 0x48, 0xC7,
+                        0xC0, 0xFF, 0xFF, 0xFF,
+                        0xFF, 0x2B, 0x05, 0xB2,
+                        0x12, 0x01, 0x00, 0xE9,
+                        0x0A, 0x09, 0x00, 0x00
+                    };
+
+                    var nmax = bytes.Length - old.Length;
+                    for (int i = 0; i < nmax; i++)
+                    {
+                        bool notfound = false;
+                        for (int j = 0; j < old.Length; j++)
+                        {
+                            byte a = bytes[i + j];
+                            byte b = old[j];
+
+                            if (a != b)
+                            {
+                                notfound = true;
+                                break;
+                            }
+                        }
+                        if (!notfound)
+                        {
+                            logger.LogInfo("    at 0x" + $"{i:X4}");
+
+                            if (!File.Exists(f2))
+                            {
+                                logger.LogInfo("    backed up");
+                                File.WriteAllBytes(f2, bytes);
+                            }
+
+                            for (int j = 0; j < old.Length; j++)
+                            {
+                                bytes[i + j] = 0x90;
+                            }
+                            logger.LogInfo("    patching");
+                            File.WriteAllBytes(f, bytes);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    logger.LogWarning("    original changed");
+                }
+
+                logger.LogInfo("    Done.");
+            }
+            else
+            {
+                logger.LogInfo("Not found");
+            }
+        }
     }
 }
